521309227aabe1a8acf6ff7d47fc32f1a0d0db83
Aidan Asher <ai.arenas@duocuc.cl>
2023-11-14 23:31:59 -0300

    Signs of all kinds

--- Serviexpress/taller/urls.py
+++ Serviexpress/taller/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
     path('index/', views.index, name='index'),
-    path('hnf/', views.hnf, name='hnf')
+    
+    path('logout/', views.signout, name='logout')
+    #path('hnf/', views.hnf, name='hnf')
 ]
--- Serviexpress/taller/views.py
+++ Serviexpress/taller/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+
 
 def home(request):
     return render(request, "home.html")
 
-def hnf(request):
-    return render(request, "hnf.html")
 
 def signup(request):
     if request.method == "GET":
         print("Desplegando formulario")
-        return render(request, "signup.html", {"form": UserCreationForm})
+        return render(request, "signup.html")
     else:
         if request.POST["password1"] == request.POST["password2"]:
             # Registro de usuario
@@ -22,19 +21,45 @@ def signup(request):
                     username=request.POST["username"],
                     password=request.POST["password1"],
                 )
-                login(request,user)
-                return redirect('index')
+                login(request, user)
+                return redirect("index")
             except:
                 return render(
                     request,
                     "signup.html",
-                    {"form": UserCreationForm, "Error": "El usuario ya existe"},
+                    {"Error": "El usuario ya existe"}
                 )
         return render(
             request,
             "signup.html",
-            {"form": UserCreationForm, "Error": "Las contraseñas no son iguales"},
+            {"Error": "Las contraseñas no son iguales"},
         )
+
+
+def signin(request):
+    if request.method == "GET":
+        print("Desplegando formulario")
+        return render(request, "signin.html")
+    else:
+        try:
+            user = User.objects.create_user(
+                username=request.POST["username"],
+                password=request.POST["password1"],
+            )
+            login(request, user)
+            return redirect("index")
+        except:
+            return render(
+                request,
+                "signup.html",
+                {"form": UserCreationForm, "Error": "El usuario ya existe"},
+            )
+
 
 def index(request):
     return render(request, "index.html")
+
+
+def signout(request):
+    logout(request)
+    return redirect("home")
